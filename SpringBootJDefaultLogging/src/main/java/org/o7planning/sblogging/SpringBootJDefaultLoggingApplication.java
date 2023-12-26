d9519174331fa07a77e100169d967a7336c2ef20
Віка Зварко <114394185+Vviktoria1@users.noreply.github.com>
2023-12-26 22:27:52 +0200

    Add files via upload

---
+++ SpringBootJDefaultLogging/src/main/java/org/o7planning/sblogging/SpringBootJDefaultLoggingApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.sblogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJDefaultLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJDefaultLoggingApplication.class, args);
+	}
+
+}
